694d0799163ae7bed3f69c895bcb3d47a067619f
Sasi kala <104757549+sasi2303@users.noreply.github.com>
2023-10-27 10:04:17 +0530

    Add files via upload

---
+++ mirror image.py
@@ -0,0 +1,6 @@
+str1=input("Enter the String:")
+rev_str =' '
+for i in str1:
+    rev_str=i+rev_str
+print(rev_str)
+
---
+++ replace string.py
@@ -0,0 +1,4 @@
+s1 = input ("Enter the string to be replaced :")
+s2 = input ("Enter the string to replace with :")
+s1 = s2
+print ("Replaced string is :" , s1)
